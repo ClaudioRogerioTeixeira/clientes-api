85c5c5255b617be081da3fde2f85e5b12763c408
Teixeira <claudioteixeira@outlook.com>
2022-04-25 11:39:59 -0300

    Configurações

---
+++ db.js
@@ -0,0 +1,9 @@
+const clientes = require("./data/clientes");
+const tipos = require("./data/tipos");
+const categorias = require("./data/categorias");
+
+module.exports = {
+  clientes,
+  tipos,
+  categorias
+}
